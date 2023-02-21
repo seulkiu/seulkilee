2b900594a5d70cabd44f4e8d71aa5d6ca5f74954
“seulkiu” <alxsm90@naver.com>
2023-02-21 23:24:35 +0900

    subpage

---
+++ src/js/sub.js
@@ -0,0 +1,7 @@
+//=========================Page Close button=========================//
+const iconBackElem = document.querySelector('.h-menu');
+
+iconBackElem.addEventListener('click', function (){
+    window.history.back();
+    // console.log('click');
+});
